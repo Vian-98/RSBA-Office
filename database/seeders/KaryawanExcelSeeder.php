97923eff0a0ea73614ab3f8ff04293d05e844085
Vian-98 <rizki.favian123@gmail.com>
2026-07-13 16:08:42 +0700

    fix: map Fika Erisandy to UGD room since IGD is named UGD in database

--- database/seeders/KaryawanExcelSeeder.php
+++ database/seeders/KaryawanExcelSeeder.php
@@ -179,8 +179,9 @@ class KaryawanExcelSeeder extends Seeder
                 $ruanganId = $ruanganTarget ? $ruanganTarget->id : $ruanganId;
                 $isCoordinator = true;
                 $emailKoor = 'koor_ugd@rsba.com';
-            } elseif ($pinAbsenStr === '22230383') { // Fika Erisandy -> IGD
-                $ruanganTarget = \App\Models\Ruangan::where('nama', 'like', '%IGD%')->first();
+            } elseif ($pinAbsenStr === '22230383') { // Fika Erisandy -> IGD (UGD)
+                $ruanganTarget = \App\Models\Ruangan::where('nama', 'like', '%IGD%')->first()
+                    ?? \App\Models\Ruangan::where('nama', 'like', '%UGD%')->first();
                 $ruanganId = $ruanganTarget ? $ruanganTarget->id : $ruanganId;
                 $isCoordinator = true;
                 $emailKoor = 'koor_igd@rsba.com';
