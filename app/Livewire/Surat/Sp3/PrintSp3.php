7897627e0ac60d7a40e5463c7b8ddb7e1ac62a00
Vian-98 <rizki.favian123@gmail.com>
2026-07-23 11:49:05 +0700

    feat(sp3): update SP3 approval logic and print layout

--- app/Livewire/Surat/Sp3/PrintSp3.php
+++ app/Livewire/Surat/Sp3/PrintSp3.php
@@ -3,9 +3,10 @@
 namespace App\Livewire\Surat\Sp3;
 
 use Livewire\Component;
-// use Milon\Barcode\DNS1D;
 use Milon\Barcode\DNS2D;
 use App\Models\Surat\SuratSp3;
+use App\Services\QrGeneratorService;
+use App\Services\DocumentSignatureService;
 use Livewire\Attributes\Computed;
 
 class PrintSp3 extends Component
@@ -36,9 +37,22 @@ class PrintSp3 extends Component
     }
 
     #[Computed]
+    public function generateHeaderQrCode()
+    {
+        $docSignService = app(DocumentSignatureService::class);
+        $p12Hash = $docSignService->ensureP12SystemSignature($this->suratSp3);
+
+        $qrService = app(QrGeneratorService::class);
+        return $qrService->generateQrPngBase64($p12Hash, 4, 4);
+    }
+
+    #[Computed]
     public function generateBarcode()
     {
         $key = $this->approvals();
+        if (empty($key) || empty($key[0]['signature'])) {
+            return $this->generateHeaderQrCode();
+        }
 
         $barcode = new DNS2D();
         return $barcode->getBarcodePNG($key[0]['signature'], 'QRCODE');
