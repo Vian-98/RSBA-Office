8605c0aa4da29771efec58faf66c5047a18d12ff
Dimas Faqih <dimasfaqih005@gmail.com>
2026-07-30 08:50:08 +0700

    fix(linter): resolve DatabaseSeeder class reference and table-sp3 view rendering

--- resources/views/livewire/surat/sp3/table-sp3.blade.php
+++ resources/views/livewire/surat/sp3/table-sp3.blade.php
@@ -1,5 +1,6 @@
 <div class="w-full">
-    {!! $this->table->toHtml() !!}
+    {{ $this->table }}
+
 
     <x-filament::modal id="modal-detail-sp3" width="max-w-4xl" :autofocus="false">
         <x-slot:heading>SP3</x-slot:heading>
