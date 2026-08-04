eaa904027fd257bc5b230ff8f5878c90e137d8d2
Vian-98 <rizki.favian123@gmail.com>
2026-08-04 12:05:26 +0700

    fix(ui): pulihkan fallback logo RSBA

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,5 @@
-<img {{ $attributes }} src="{{ asset($rs?->logo ? 'storage/' . $rs->logo : 'img/logo.png') }}" alt="logo-{{ $rs?->singkatan ?? 'RSBA' }}" />
+@if ($rs?->logo)
+    <img {{ $attributes }} src="{{ asset('storage/' . $rs->logo) }}" alt="logo-{{ $rs->singkatan ?? 'RSBA' }}" />
+@else
+    <img {{ $attributes }} src="{{ asset('logo-fallback.png') }}" alt="logo-RSBA" />
+@endif
