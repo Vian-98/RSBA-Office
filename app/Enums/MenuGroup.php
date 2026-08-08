73680193f03610919e28a500f8bee2b05a3acfa2
Febri Nahrul Kha <fn.febriii@gmail.com>
2026-08-08 20:30:12 +0700

    Revert changes
    - sidebar navbar
    - menu group enum

--- app/Enums/MenuGroup.php
+++ app/Enums/MenuGroup.php
@@ -5,7 +5,8 @@ namespace App\Enums;
 enum MenuGroup: string
 {
     case NULL = '';
-    case ADM = 'admin';
+    case ADMIN = 'admin';
+    case ADM = 'adm';
     case SDM = 'sdm';
     case UMU = 'umu';
     case KEU = 'keu';
@@ -18,7 +19,8 @@ enum MenuGroup: string
     public function nama(): string
     {
         return match ($this) {
-            self::ADM => 'Administrator',
+            self::ADMIN => 'Administrator',
+            self::ADM => 'Administrasi',
             self::SDM => 'Kepegawaian',
             self::UMU => 'Umum',
             self::KEU => 'Keuangan',
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,19 +6,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ isset($title) ? config('app.name') . " | $title" : config('app.name') }}</title>
-        
-        <!-- Premium Google Fonts -->
+
+        {{-- <!-- Premium Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-        
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"> --}}
+
         <style>
             [x-cloak] {
                 display: none !important;
             }
-            body {
+
+            /* body {
                 font-family: 'Plus Jakarta Sans', sans-serif !important;
-            }
+            } */
         </style>
         <script>
             document.addEventListener('alpine:init', () => {
@@ -51,15 +52,15 @@
                 <livewire:Partials.Sidebar key="sidebar" />
 
                 <!-- Main content -->
-                <div id="main-content" class="max-h-screen flex-1 flex flex-col overflow-hidden transition-all duration-300">
+                <div id="main-content" class="flex max-h-screen flex-1 flex-col overflow-hidden transition-all duration-300">
 
                     {{-- NAVBAR --}}
-                    <div class="h-16 rounded-md bg-white shadow-md shrink-0 mx-6 mt-6">
+                    <div class="mx-6 mt-6 h-16 shrink-0 rounded-md bg-white shadow-md">
                         <livewire:Partials.Navbar :title="isset($title) ? $title : config('app.name')" key="navbar" />
                     </div>
 
                     {{-- CONTENT --}}
-                    <main id="main" class="flex-1 overflow-y-auto scrollbar-hidden px-6 py-4">
+                    <main id="main" class="scrollbar-hidden flex-1 overflow-y-auto px-6 py-4">
                         {{ $slot }}
                     </main>
 
